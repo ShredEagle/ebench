8624e02472933fba97ff4f0c30b4bb20c9c2ffa2
Adrien <Adnn@users.noreply.github.com>
2020-09-23 18:50:13 +0200

    FamilyPoly: implement addIfMatch() and removeIfPresent()

--- src/libs/aunteater/aunteater/FamilyPoly.h
+++ src/libs/aunteater/aunteater/FamilyPoly.h
@@ -2,23 +2,48 @@
 
 
 #include "Archetype.h"
+#include "Engine.h" // coming real close to circular inclusion here...
 #include "globals.h"
 //#include "templating.h"
 
 
 #include <memory>
+#include <iostream>
 
 
 namespace aunteater {
 
+class FamilyPoly;
+
 struct FamilyBase
 {
     virtual ~FamilyBase() = default;
+
+    virtual std::size_t size() const noexcept = 0;
+
+    virtual void addIfMatch(weak_entity<FamilyPoly> aEntity) = 0;
+    virtual void removeIfPresent(entity_id<FamilyPoly> aEntity) = 0;
 };
 
 
 template <class T_archetype>
 class FamilyTyped;
+
+
+
+template <class T_componentHead, class... VT_componentTail>
+bool hasAllComponents(weak_entity<FamilyPoly> aEntity)
+{
+    return aEntity->has<T_componentHead>()
+           && hasAllComponents<VT_componentTail...>(aEntity);
+}
+
+template <void * Dummy = nullptr>
+bool hasAllComponents(weak_entity<FamilyPoly> aEntity)
+{
+    return true;
+}
+
 
 template <class... VT_component>
 class FamilyTyped<Archetype<VT_component...>> : public FamilyBase
@@ -40,11 +65,36 @@ public:
     auto cend() const noexcept
     { return mNodes.cend(); }
 
-    std::size_t size() const noexcept
+    std::size_t size() const noexcept override
     { return mNodes.size(); }
+
+    void addIfMatch(weak_entity<FamilyPoly> aEntity) override
+    {
+        if (hasAllComponents<VT_component...>(aEntity))
+        {
+            mNodes.emplace_back(&(aEntity->get<VT_component>())...);
+            if (!mEntitiesPositions.emplace(entityIdFrom(*aEntity), mNodes.size()-1).second)
+            {
+                throw std::logic_error("Entity already present in this family.");
+            }
+            //broadcastNotification(&FamilyObserver<Family>::addedEntity, **insertedIt);
+        }
+    }
+
+    void removeIfPresent(entity_id<FamilyPoly> aEntity) override
+    {
+        auto foundIt = mEntitiesPositions.find(entityIdFrom(*aEntity));
+        if (foundIt != mEntitiesPositions.end())
+        {
+            //broadcastNotification(&FamilyObserver<Family>::removedEntity, **(foundIt->second));
+            mNodes.erase(mNodes.begin() + foundIt->second);
+            mEntitiesPositions.erase(foundIt);
+        }
+    }
 
 private:
     std::vector<Node> mNodes;
+    std::map<entity_id<FamilyPoly>, typename std::vector<Node>::size_type> mEntitiesPositions;
 };
 
 
@@ -56,6 +106,8 @@ public:
     {
         return FamilyPoly{std::make_unique<FamilyTyped<T_archetype>>()};
     }
+
+    std::size_t size() const noexcept;
 
     // TODO
     //auto begin() noexcept;
@@ -70,8 +122,9 @@ public:
     void addIfMatch(weak_entity<FamilyPoly> aEntity);
     void removeIfPresent(entity_id<FamilyPoly> aEntity);
 
-    void componentAddedToEntity(weak_entity<FamilyPoly> aEntity, ComponentTypeId aComponent);
-    void componentRemovedFromEntity(entity_id<FamilyPoly> aEntity, ComponentTypeId aComponent);
+    // TODO
+    //void componentAddedToEntity(weak_entity<FamilyPoly> aEntity, ComponentTypeId aComponent);
+    //void componentRemovedFromEntity(entity_id<FamilyPoly> aEntity, ComponentTypeId aComponent);
 
     std::unique_ptr<FamilyBase> mDerived{nullptr};
 
@@ -82,16 +135,25 @@ private:
 };
 
 
-// TODO
 inline void FamilyPoly::addIfMatch(weak_entity<FamilyPoly> aEntity)
-{}
-inline void FamilyPoly::removeIfPresent(entity_id<FamilyPoly> aEntity)
-{}
+{
+    mDerived->addIfMatch(aEntity);
+}
 
-inline void FamilyPoly::componentAddedToEntity(weak_entity<FamilyPoly> aEntity, ComponentTypeId aComponent)
-{}
-inline void FamilyPoly::componentRemovedFromEntity(entity_id<FamilyPoly> aEntity, ComponentTypeId aComponent)
-{}
+inline void FamilyPoly::removeIfPresent(entity_id<FamilyPoly> aEntity)
+{
+    mDerived->removeIfPresent(aEntity);
+}
+
+inline std::size_t FamilyPoly::size() const noexcept
+{
+    return mDerived->size();
+}
+
+//inline void FamilyPoly::componentAddedToEntity(weak_entity<FamilyPoly> aEntity, ComponentTypeId aComponent)
+//{}
+//inline void FamilyPoly::componentRemovedFromEntity(entity_id<FamilyPoly> aEntity, ComponentTypeId aComponent)
+//{}
 
 
 } // namespace aunteater
