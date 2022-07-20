3b341805b0bd9ee6e3fb474462982939241f5849
Francois Poizat <francois.poizat@gmail.com>
2022-07-20 16:09:53 +0200

    added RemoveEntity benchmark

--- src/libs/ebench/ebench/bench.h
+++ src/libs/ebench/ebench/bench.h
@@ -1,11 +1,27 @@
+#include <iostream>
+#include <chrono>
 #include <math/Vector.h>
 
 #include <benchmark/benchmark.h>
 
 namespace ad {
 namespace ebench {
+
     template<typename T_ecs>
     void BM_AddEntity(benchmark::State & aState)
+    {
+        T_ecs world;
+
+        for (auto _ : aState)
+        {
+            world.addEntityWithPosition();
+        }
+
+        aState.SetItemsProcessed(aState.iterations());
+    }
+
+    template<typename T_ecs>
+    void BM_AddEntityWithSystem(benchmark::State & aState)
     {
         T_ecs world;
         world.template addSystem<typename T_ecs::MovementSystem>();
@@ -16,6 +32,38 @@ namespace ebench {
         }
 
         aState.SetItemsProcessed(aState.iterations());
+    }
+
+    template<typename T_ecs, int N_toRemove>
+    void BM_RemoveEntity(benchmark::State & aState)
+    {
+        T_ecs world;
+        for (auto _ : aState)
+        {
+            std::vector<typename T_ecs::Entity> entities;
+            for (int i = 0; i < N_toRemove; i++)
+            {
+                entities.push_back(world.addEntity());
+            }
+
+            auto start = std::chrono::high_resolution_clock::now();
+
+            for (auto entityToRemove : entities)
+            {
+                world.remove(entityToRemove);
+            }
+
+            world.update();
+
+            auto end = std::chrono::high_resolution_clock::now();
+
+            auto elapsed_seconds =
+                std::chrono::duration_cast<std::chrono::duration<double>>(
+                        end - start);
+
+            aState.SetIterationTime(elapsed_seconds.count());
+        }
+        aState.SetItemsProcessed(aState.iterations() * N_toRemove);
     }
 
     template<typename T_ecs>
@@ -56,7 +104,7 @@ namespace ebench {
     }
 
     template<typename T_ecs, int N>
-    void BM_AccessComponentInternal(benchmark::State & aState)
+    void BM_RandomDynamicAccessComponent(benchmark::State & aState)
     {
         T_ecs world;
         world.template addSystem<typename T_ecs::template RandomAccessSystem<N>>();
@@ -68,34 +116,22 @@ namespace ebench {
         }
     }
 
-    template<typename T_ecs>
-    void BM_AccessComponent(benchmark::State & aState)
-    {
-        switch (aState.range(0))
-        {
-            case 4:
-                BM_AccessComponentInternal<T_ecs, 4>(aState);
-                break;
-            case 4 * 4:
-                BM_AccessComponentInternal<T_ecs, 4 * 4>(aState);
-                break;
-            case 4 * 16:
-                BM_AccessComponentInternal<T_ecs, 4 * 16>(aState);
-                break;
-            case 4 * 64:
-                BM_AccessComponentInternal<T_ecs, 4 * 64>(aState);
-                break;
-        }
-    }
-
-
     template<typename T>
     int allbench()
     {
         BENCHMARK_TEMPLATE(BM_AddEntity, T);
+        BENCHMARK_TEMPLATE(BM_AddEntityWithSystem, T);
+        BENCHMARK_TEMPLATE(BM_RemoveEntity, T, 1)->UseManualTime();
+        BENCHMARK_TEMPLATE(BM_RemoveEntity, T, 16)->UseManualTime();
+        BENCHMARK_TEMPLATE(BM_RemoveEntity, T, 32)->UseManualTime();
+        BENCHMARK_TEMPLATE(BM_RemoveEntity, T, 64)->UseManualTime();
+        BENCHMARK_TEMPLATE(BM_RemoveEntity, T, 128)->UseManualTime();
         BENCHMARK_TEMPLATE(BM_UpdateSystem, T)->Range(2 << 9, 2 << 16);
         BENCHMARK_TEMPLATE(BM_AddComponent, T);
-        BENCHMARK_TEMPLATE(BM_AccessComponent, T)->RangeMultiplier(4)->Range(4, 256);
+        BENCHMARK_TEMPLATE(BM_RandomDynamicAccessComponent, T, 4);
+        BENCHMARK_TEMPLATE(BM_RandomDynamicAccessComponent, T, 16);
+        BENCHMARK_TEMPLATE(BM_RandomDynamicAccessComponent, T, 64);
+        BENCHMARK_TEMPLATE(BM_RandomDynamicAccessComponent, T, 256);
         return 0;
     }
 }
