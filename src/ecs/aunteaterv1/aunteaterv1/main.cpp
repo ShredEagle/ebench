7d0ffb995353105c710c8171cbfe72f68428b207
Francois Poizat <francois.poizat@gmail.com>
2022-10-27 15:03:56 +0200

    fix aunteater benchmark

--- src/ecs/aunteaterv1/aunteaterv1/main.cpp
+++ src/ecs/aunteaterv1/aunteaterv1/main.cpp
@@ -1,18 +1,9 @@
-
 #include <aunteaterAdapter/world.h>
 
 #include <ebench/bench.h>
 #include <benchmark/benchmark.h>
 #include <string>
 
-
-namespace ad
-{
-namespace ebench
-{
-
-static int yo = allbench<AunteaterWorld>();
+static int yo = ad::ebench::allbench<ad::ebench::AunteaterWorld>();
 
 BENCHMARK_MAIN();
-}
-}
